1063b9cafa34d02f6fa649e418559deb2eede8fa
Jesko Hecking-Harbusch <13851486+jeskoheha@users.noreply.github.com>
2017-03-13 14:22:58 +0100

    JHH: first part of introducing bounded synthesis to ADAM

--- src/uniolunisaar/adam/bounded/qbfapproach/solver/QBFSolver.java
+++ src/uniolunisaar/adam/bounded/qbfapproach/solver/QBFSolver.java
@@ -1,13 +1,394 @@
 package uniolunisaar.adam.bounded.qbfapproach.solver;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import uniol.apt.adt.pn.Marking;
+import uniol.apt.adt.pn.PetriNet;
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.pn.Transition;
+import uniol.apt.util.Pair;
 import uniolunisaar.adam.bounded.qbfapproach.petrigame.QBFPetriGame;
 import uniolunisaar.adam.ds.solver.Solver;
 import uniolunisaar.adam.ds.winningconditions.WinningCondition;
 
 public abstract class QBFSolver<W extends WinningCondition> extends Solver<QBFPetriGame, W, QBFSolverOptions> {
+	private Map<Transition, Set<Place>> restCache = new HashMap<>(); // proven to be slightly useful in terms of performance
+	private Map<Transition, Set<Place>> preMinusPostCache = new HashMap<>();
+	
+	protected QBFPetriGame pg;
+	protected PetriNet pn;
+	protected BufferedWriter writer;
+	protected int variablesCounter = 2; // 1 reserved for phi
+	protected Map<String, Integer> numbersForVariables = new HashMap<>(); // map for storing keys and the corresponding value
+	
+	protected Transition[] transitions;
+	protected int[] flowSubFormulas;
+	protected int[] deadlockSubFormulas;
+	protected int[] terminatingSubFormulas;
+	protected File file = null;
+	
+	// TODO folgendes schön aufteilen
+	protected String linebreak = "\n\n"; // Controller
+	protected String additionalSystemName = "AS___"; //Controller
+	protected int n = 10;
 
-    public QBFSolver(QBFPetriGame game, W winCon, QBFSolverOptions so) {
-        super(game, winCon, so);
-    }
+	public QBFSolver(QBFPetriGame game, W winCon, QBFSolverOptions so) {
+		super(game, winCon, so);
+		pg = game;
+		pn = pg.getNet();
+		transitions = new Transition[pn.getTransitions().size()];
+		int counter = 0;
+		for (Transition t : pn.getTransitions()) {
+			transitions[counter++] = t;
+		}
 
+		// Create random file in tmp directory which is deleted after solving it
+		String prefix = "";
+		final Random rand = new Random();
+		final String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345674890";
+		for (int i = 0; i < 20; ++i) {
+			prefix += lexicon.charAt(rand.nextInt(lexicon.length()));
+		}
+		try {
+			file = File.createTempFile(prefix, ".txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		file.deleteOnExit();
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getInitial() {
+		Marking initialMarking = pg.getNet().getInitialMarking();
+		Set<Integer> initial = new HashSet<>();
+		for (Place p : pn.getPlaces()) {
+			if (initialMarking.getToken(p).getValue() == 1) {
+				initial.add(getVarNr(p.getId() + "." + 1, true));
+			} else {
+				initial.add(-getVarNr(p.getId() + "." + 1, true));
+			}
+		}
+		return writeAnd(initial);
+	}
+	
+	public String[] getDeadlock() throws IOException {
+		writeDeadlockSubFormulas(1, n);
+		String[] deadlock = new String[n + 1];
+		Set<Integer> and = new HashSet<>();
+		for (int i = 1; i <= n; ++i) {
+			and.clear();
+			for (int j = 0; j < pn.getTransitions().size(); ++j) {
+				and.add(deadlockSubFormulas[pn.getTransitions().size() * (i - 1) + j]);
+			}
+			deadlock[i] = writeAnd(and);
+		}
+		return deadlock;
+	}
+
+	private void writeDeadlockSubFormulas(int s, int e) throws IOException {
+		Transition t;
+		Set<Integer> or = new HashSet<>();
+		int number;
+		int strat;
+		for (int i = s; i <= e; ++i) {
+			for (int j = 0; j < pn.getTransitions().size(); ++j) {
+				t = transitions[j];
+				number = createUniqueID();
+				or.clear();
+				for (Place p : t.getPreset()) {
+					or.add(-getVarNr(p.getId() + "." + i, true)); // "p.i"
+					strat = addSysStrategy(p, t);
+					if (strat != 0)
+						or.add(-strat);
+				}
+				writer.write(number + " = " + writeOr(or));
+				deadlockSubFormulas[pn.getTransitions().size() * (i - 1) + j] = number;
+			}
+		}
+	}
+	
+	public String[] getFlow() throws IOException {
+		// writeFlowSubFormulas(); // slower
+		String[] flow = new String[n + 1];
+		Set<Integer> or = new HashSet<>();
+		for (int i = 1; i < n; ++i) {
+			or.clear();
+			for (int j = 0; j < pn.getTransitions().size(); ++j) {
+				// or.add(flowSubFormulas[pn.getTransitions().size() * (i - 1) + j]);
+				or.add(getOneTransition(transitions[j], i));
+			}
+			flow[i] = writeOr(or);
+		}
+		return flow;
+	}
+	
+	public int getOneTransition(Transition t, int i) throws IOException {
+		int number = createUniqueID();
+		Set<Integer> and = new HashSet<>();
+		int strat;
+		for (Place p : t.getPreset()) {
+			and.add(getVarNr(p.getId() + "." + i, true));
+			strat = addSysStrategy(p, t);
+			if (strat != 0)
+				and.add(strat);
+		}
+		for (Place p : t.getPostset()) {
+			and.add(getVarNr(p.getId() + "." + (i + 1), true));
+		}
+		// Slight performance gain by using these caches
+		Set<Place> rest = restCache.get(t);
+		if (rest == null) {
+			rest = new HashSet<>(pg.getNet().getPlaces());
+			rest.removeAll(t.getPreset());
+			rest.removeAll(t.getPostset());
+			restCache.put(t, rest);
+		}
+
+		for (Place p : rest) {
+			int p_i = getVarNr(p.getId() + "." + i, true);
+			int p_iSucc = getVarNr(p.getId() + "." + (i + 1), true);
+			and.add(writeImplication(p_i, p_iSucc));
+			and.add(writeImplication(p_iSucc, p_i));
+		}
+
+		Set<Place> preMinusPost = preMinusPostCache.get(t);
+		if (preMinusPost == null) {
+			preMinusPost = new HashSet<>(t.getPreset());
+			preMinusPost.removeAll(t.getPostset());
+			preMinusPostCache.put(t, preMinusPost);
+		}
+		for (Place p : preMinusPost) {
+			and.add(-getVarNr(p.getId() + "." + (i + 1), true));
+		}
+		writer.write(number + " = " + writeAnd(and));
+		return number;
+	}
+	
+	public String[] getTerminating() throws IOException {
+		writeTerminatingSubFormulas(1, n);
+		String[] terminating = new String[n + 1];
+		Set<Integer> and = new HashSet<>();
+		for (int i = 1; i <= n; ++i) {
+			if (pg.getNet().getTransitions().size() >= 1) {
+				and.clear();
+				for (int j = 0; j < pn.getTransitions().size(); ++j) {
+					and.add(terminatingSubFormulas[pn.getTransitions().size() * (i - 1) + j]);
+				}
+				terminating[i] = writeAnd(and);
+			} else {
+				terminating[i] = "";
+			}
+		}
+		return terminating;
+	}
+	
+	private void writeTerminatingSubFormulas(int s, int e) throws IOException {
+		Set<Integer> or = new HashSet<>();
+		Set<Place> pre;
+		Transition t;
+		int key;
+		for (int i = s; i <= e; ++i) {
+			for (int j = 0; j < pn.getTransitions().size(); ++j) {
+				t = transitions[j];
+				key = createUniqueID();
+				pre = t.getPreset();
+				or.clear();
+				for (Place p : pre) {
+					or.add(-getVarNr(p.getId() + "." + i, true));
+				}
+				writer.write(key + " = " + writeOr(or));
+				terminatingSubFormulas[pn.getTransitions().size() * (i - 1) + j] = key;
+			}
+		}
+	}
+	
+	public String[] getDeterministic() throws IOException { // faster than naive implementation
+		List<Set<Integer>> and = new ArrayList<>(n + 1);
+		and.add(null); // first element is never accessed
+		for (int i = 1; i <= n; ++i) {
+			and.add(new HashSet<>());
+		}
+		Transition t1, t2;
+		Transition[] sys_transitions;
+		for (Place sys : pn.getPlaces()) {
+			// Additional system places are not forced to behave deterministically, this is the faster variant (especially the larger the PG becomes)
+			if (!pg.getEnvPlaces().contains(sys) && !sys.getId().startsWith(additionalSystemName)) {
+				if (sys.getPostset().size() > 1) {
+					sys_transitions = sys.getPostset().toArray(new Transition[0]);
+					for (int j = 0; j < sys_transitions.length; ++j) {
+						t1 = sys_transitions[j];
+						for (int k = j + 1; k < sys_transitions.length; ++k) {
+							t2 = sys_transitions[k];
+							for (int i = 1; i <= n; ++i) {
+								and.get(i).add(writeOneMissingPre(t1, t2, i));
+							}
+						}
+					}
+				}
+			}
+		}
+		String[] deterministic = new String[n + 1];
+		for (int i = 1; i <= n; ++i) {
+			deterministic[i] = writeAnd(and.get(i));
+		}
+		return deterministic;
+	}
+	
+	private int writeOneMissingPre(Transition t1, Transition t2, int i) throws IOException {
+		Set<Integer> or = new HashSet<>();
+		int strat;
+		for (Place p : t1.getPreset()) {
+			or.add(-getVarNr(p.getId() + "." + i, true));
+			strat = addSysStrategy(p, t1);
+			if (strat != 0)
+			or.add(-strat);
+		}
+		for (Place p : t2.getPreset()) {
+			or.add(-getVarNr(p.getId() + "." + i, true));
+			strat = addSysStrategy(p, t2);
+			if (strat != 0)
+				or.add(-strat);
+		}
+
+		int number = createUniqueID();
+		writer.write(number + " = " + writeOr(or));
+		return number;
+	}
+	
+	public String getLoopIJ() throws IOException {
+		Set<Integer> or = new HashSet<>();
+		for (int i = 1; i < n; ++i) {
+			for (int j = i + 1; j <= n; ++j) {
+				Set<Integer> and = new HashSet<>();
+				for (Place p : pn.getPlaces()) {
+					int p_i = getVarNr(p.getId() + "." + i, true);
+					int p_j = getVarNr(p.getId() + "." + j, true);
+					and.add(writeImplication(p_i, p_j));
+					and.add(writeImplication(p_j, p_i));
+				}
+				int andNumber = createUniqueID();
+				writer.write(andNumber + " = " + writeAnd(and));
+				or.add(andNumber);
+			}
+		}
+		return writeOr(or);
+	}
+	
+	public int addSysStrategy(Place p, Transition t) {
+		if (!pg.getEnvPlaces().contains(p)) {
+			if (p.getId().startsWith(additionalSystemName)) {
+				return getVarNr(p.getId() + ".." + t.getId(), true);
+			} else {
+				return getVarNr(p.getId() + ".." + getTruncatedId(t.getId()), true);
+			}
+		} else {
+			return 0;
+		}
+	}
+
+	public int getVarNr(String id, boolean extraCheck) {
+		Integer ret = numbersForVariables.get(id);
+		if (ret != null) {
+			return ret;
+		} else {
+			if (extraCheck) {
+				throw new IllegalArgumentException("Could not but should have found: " + id);
+			} else {
+				return createVariable(id);
+			}
+		}
+	}
+
+	public Pair<Boolean, Integer> getVarNrWithResult(String id) {
+		Integer ret = numbersForVariables.get(id);
+		if (ret != null) {
+			return new Pair<>(false, ret);
+		} else {
+			return new Pair<>(true, createVariable(id));
+		}
+	}
+
+	public int createVariable(String id) {
+		numbersForVariables.put(id, variablesCounter);
+		return variablesCounter++;
+	}
+
+	public int createUniqueID() {
+		return variablesCounter++;
+	}
+	
+	public String getTruncatedId(String id) {
+		int index = id.indexOf("__");
+		if (index != -1) {
+			id = id.substring(0, index);
+		}
+		return id;
+	}
+
+
+	// WRITERS:
+	public String writeExists(Set<Integer> input) {
+		return writeString("exists", input);
+	}
+
+	public String writeForall(Set<Integer> input) {
+		return writeString("forall", input);
+	}
+
+	public String writeOr(Set<Integer> input) {
+		return writeString("or", input);
+	}
+
+	public String writeAnd(Set<Integer> input) {
+		return writeString("and", input);
+	}
+
+	private String writeString(String op, Set<Integer> input) {
+		StringBuilder sb = new StringBuilder(op);
+		sb.append("(");
+		String delim = ""; // first element is added without ","
+
+		for (int i : input) {
+			sb.append(delim);
+			delim = ",";
+			sb.append(i);
+		}
+		sb.append(")" + linebreak);
+		String result = sb.toString();
+		return result;
+	}
+
+	public int writeImplication(int from, int to) throws IOException {
+		Pair<Boolean, Integer> result = getVarNrWithResult("Variable:" + from + "=>Variable:" + to);
+		int number = result.getSecond();
+		if (result.getFirst()) {
+			Set<Integer> or = new HashSet<>();
+			or.add(-from);
+			or.add(to);
+			writer.write(number + " = " + writeOr(or));
+		}
+		return number;
+	}
 }
