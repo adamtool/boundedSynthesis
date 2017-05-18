1bdbe2c00716f7be5310781ca1b712f96619399f
Manuel Gieseking <mgieseking@users.noreply.github.com>
2017-05-18 18:08:59 +0200

    MG: adapted security system

---
+++ test/uniolunisaar/adam/bounded/qbfapproach/SSTest.java
@@ -0,0 +1,73 @@
+package uniolunisaar.adam.bounded.qbfapproach;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import uniol.apt.adt.pn.PetriNet;
+import uniol.apt.module.exception.ModuleException;
+import uniolunisaar.adam.bounded.qbfapproach.solver.QBFSolver;
+import uniolunisaar.adam.bounded.qbfapproach.solver.QBFSolverFactory;
+import uniolunisaar.adam.bounded.qbfapproach.solver.QBFSolverOptions;
+import uniolunisaar.adam.ds.exceptions.NetNotConcurrencyPreservingException;
+import uniolunisaar.adam.ds.exceptions.NetNotSafeException;
+import uniolunisaar.adam.ds.exceptions.NoStrategyExistentException;
+import uniolunisaar.adam.ds.exceptions.CouldNotFindSuitableWinningConditionException;
+import uniolunisaar.adam.ds.exceptions.NoSuitableDistributionFoundException;
+import uniolunisaar.adam.ds.exceptions.SolverDontFitPetriGameException;
+import uniolunisaar.adam.ds.exceptions.UnboundedPGException;
+import uniolunisaar.adam.ds.winningconditions.WinningCondition;
+import uniolunisaar.adam.generators.ContainerTerminal;
+import uniolunisaar.adam.generators.SecuritySystem;
+import uniolunisaar.adam.tools.Logger;
+import uniolunisaar.adam.tools.Tools;
+
+/**
+ *
+ * @author Manuel Gieseking
+ */
+@Test
+public class SSTest {
+
+    private static final String outputDir = System.getProperty("testoutputfolder") + "/securitySystem/";
+
+    private static final int countSecuritySystems = 7;
+
+    @BeforeClass
+    public void createFolder() {
+        Logger.getInstance().setVerbose(false);
+        (new File(outputDir)).mkdirs();
+    }
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Container terminal (more than one env place)
+    @DataProvider(name = "secSystem")
+    public static Object[][] securitySystem() {
+        Object[][] out = new Object[countSecuritySystems][2];
+        for (int i = 0; i < countSecuritySystems; i++) {
+            out[i][0] = i + 2;
+            out[i][1] = true;
+        }
+        return out;
+    }
+
+    @Test(dataProvider = "secSystem")
+    public void testSecuritySystem(int intrudingPoints, boolean hasStrategy) throws NetNotSafeException, NetNotConcurrencyPreservingException, NoStrategyExistentException, IOException, InterruptedException, FileNotFoundException, ModuleException, NoSuitableDistributionFoundException, SolverDontFitPetriGameException, UnboundedPGException, CouldNotFindSuitableWinningConditionException {
+        final String path = outputDir;
+        String name = intrudingPoints + "_secSystem";
+        File f = new File(path);
+        f.mkdir();
+        System.out.println("Generate security system ...");
+        PetriNet pn = SecuritySystem.createSafetyVersionForBounded(intrudingPoints, true);
+        Tools.savePN2PDF(path + name, pn, false);
+        QBFSolver<? extends WinningCondition> solv = QBFSolverFactory.getInstance().getSolver(pn, false, new QBFSolverOptions(7, 3));
+        if (hasStrategy) {
+            Assert.assertTrue(solv.existsWinningStrategy());
+            Tools.savePN2PDF(path + name + "_pg", solv.getStrategy(), true);
+        } else {
+            Assert.assertFalse(solv.existsWinningStrategy());
+        }
+    }
+}
