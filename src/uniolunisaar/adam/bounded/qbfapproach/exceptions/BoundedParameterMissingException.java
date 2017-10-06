b46701c763f9554f2110637ad9cf7de5be3bc1f1
Manuel Gieseking <mgieseking@users.noreply.github.com>
2017-10-06 18:02:12 +0200

    MG: forgotten exception added.

---
+++ src/uniolunisaar/adam/bounded/qbfapproach/exceptions/BoundedParameterMissingException.java
@@ -0,0 +1,17 @@
+package uniolunisaar.adam.bounded.qbfapproach.exceptions;
+
+import uniolunisaar.adam.ds.exceptions.ParameterMissingException;
+
+/**
+ *
+ * @author Manuel Gieseking
+ */
+public class BoundedParameterMissingException extends ParameterMissingException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BoundedParameterMissingException(int n, int b) {
+        super("No suitable parametes found: n=" + n + ", b=" + b);
+    }
+
+}
