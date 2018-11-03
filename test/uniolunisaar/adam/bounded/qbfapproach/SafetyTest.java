67064aa6b5454a469ac96976ab5cab7ca69a82c5
Jesko Hecking-Harbusch <13851486+jeskoheha@users.noreply.github.com>
2018-11-03 12:19:28 +0100

    JHH: add PN translation of SDN encoding

--- test/uniolunisaar/adam/bounded/qbfapproach/SafetyTest.java
+++ test/uniolunisaar/adam/bounded/qbfapproach/SafetyTest.java
@@ -1,8 +1,21 @@
 package uniolunisaar.adam.bounded.qbfapproach;
 
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import uniolunisaar.adam.ds.petrigame.PetriGame;
+import uniolunisaar.adam.logic.util.AdamTools;
+import uniolunisaar.adam.sdnencoding.SequentialUpdate;
+import uniolunisaar.adam.sdnencoding.SwitchUpdate;
+import uniolunisaar.adam.sdnencoding.TopologyToPN;
+import uniolunisaar.adam.sdnencoding.Update;
 
 @Test
 public class SafetyTest extends EmptyTest {
@@ -12,16 +25,46 @@ public class SafetyTest extends EmptyTest {
         if (System.getProperty("examplesfolder") == null) {
         	System.setProperty("examplesfolder", "examples");
         }
+        if (System.getProperty("PROPERTY_FILE") == null) {
+        	System.setProperty("PROPERTY_FILE", "ADAM.properties");
+        }
     }
 	
 	@Test(timeOut = 1800 * 1000) // 30 min
 	public void testForallSafety() throws Exception {
+		Set<String> ingress = new HashSet<>();
+		ingress.add("sw0");
+		List<Update> updateList = new ArrayList<>();
+		updateList.add(new SwitchUpdate("sw8", "sw7", "sw5"));
+		updateList.add(new SwitchUpdate("sw9", "sw10", "sw8"));
+		updateList.add(new SwitchUpdate("sw0", "sw1", "sw2"));
+		SequentialUpdate update = new SequentialUpdate(updateList);
+		TopologyToPN ttp = new TopologyToPN(new File("examples/AbilenePN.txt"), ingress, update);
+		PetriGame pn = ttp.generatePetriNet();
+		//initial configuration for forwarding
+		pn.getPlace("sw0fwdTosw1").setInitialToken(1);
+		pn.getPlace("sw1fwdTosw10").setInitialToken(1);
+		pn.getPlace("sw10fwdTosw7").setInitialToken(1);
+		pn.getPlace("sw7fwdTosw6").setInitialToken(1);
+		pn.getPlace("sw6fwdTosw4").setInitialToken(1);
+		pn.getPlace("sw4fwdTosw5").setInitialToken(1);
+		
+		//initial configuration for updating
+		pn.getPlace("sw8fwdTosw7").setInitialToken(1);
+		pn.getPlace("sw9fwdTosw10").setInitialToken(1);
+		
+		pn.getPlace("sw2fwdTosw9").setInitialToken(1);
+		
+		AdamTools.savePG2PDF("test", pn, false);
+		AdamTools.saveAPT("example", pn, false);
+		
 		//oneTest("tests/watchdog5", 15, 3, true);		// TODO search for bounds
 		//oneTest("container/container", 10, 2, true);	// TODO search for bounds
 		//oneTest("notConcurrencyPreservingTests/toMakeCP", 20, 2, false);	// TODO nets are unsafe, making them safe defeats their purpose
 		//oneTest("notConcurrencyPreservingTests/madeCP", 20, 2, true);		// TODO nets are unsafe, making them safe defeats their purpose
-		oneTest("ndet/nondet_s3", 10, 0, false);
-		oneTest("ndet/nondet_s3", 10, 2, true);
+		oneTest("jhh/twoIndependentProcesses", 7, 0, true);
+		//oneTest("ndet/nondet_s3", 10, 0, false);
+		/*oneTest("ndet/nondet_s3", 10, 2, true);
 		oneTest("ndet/nondet_s3_noStrat", 15, 2, false);
 		oneTest("ndet/nondet_unnecessarily_noStrat", 15, 3, false);
 		oneTest("ndet/nondet_withBad", 12, 2, false);
