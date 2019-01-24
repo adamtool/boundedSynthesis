368f15dc51ebfa256646d034fe01bbdefb9316d4
Jesko Hecking-Harbusch <13851486+jeskoheha@users.noreply.github.com>
2019-01-24 17:01:40 +0100

    JHH: access control small changes

--- test/uniolunisaar/adam/bounded/qbfapproach/SafetyTest.java
+++ test/uniolunisaar/adam/bounded/qbfapproach/SafetyTest.java
@@ -3,6 +3,9 @@ package uniolunisaar.adam.bounded.qbfapproach;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import uniolunisaar.adam.generators.pnwt.util.acencoding.OfficeBenchmark;
+import uniolunisaar.adam.util.PNWTTools;
+
 /**
  * 
  * @author Jesko Hecking-Harbusch
@@ -63,6 +66,8 @@ public class SafetyTest extends EmptyTest {
 	
 	@Test(timeOut = 1800 * 1000) // 30 min
 	public void testGeneralExamples() throws Exception {
+		PNWTTools.saveAPT("deploy/accesscontrolToy", OfficeBenchmark.generateOfficeToy(), false);
+		PNWTTools.saveAPT("deploy/accesscontrolSmall", OfficeBenchmark.generateOfficeSmall(), false);
 		//oneTest("tests/watchdog5", 15, 3, true);		// TODO search for bounds
 		//oneTest("container/container", 10, 2, true);	// TODO search for bounds
 		//oneTest("notConcurrencyPreservingTests/toMakeCP", 20, 2, false);	// TODO nets are unsafe, making them safe defeats their purpose
